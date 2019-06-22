ebac037877a41474aa3316aeed24c3555c40d2ef
Andmedoctopus <andmed999@gmail.com>
2019-06-22 12:40:51 +0300

    Added qustions and choices to admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'was_published_recently', 'pub_date')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
